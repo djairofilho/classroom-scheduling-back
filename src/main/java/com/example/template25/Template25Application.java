68acbcc0eef02f88d8b8bbe72158cbd79a8a822d
djairofilho <djairodsfilho@gmail.com>
2026-03-21 21:45:02 -0300

    chore: update for jdk25

---
+++ src/main/java/com/example/template25/Template25Application.java
@@ -0,0 +1,13 @@
+package com.example.template25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Template25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Template25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/template25/Template25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.template25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Template25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
