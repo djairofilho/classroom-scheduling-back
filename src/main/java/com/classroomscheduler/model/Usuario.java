7bfae4f16887fe0edabfdb302fbfafb8af113b2d
djairofilho <djairodsfilho@gmail.com>
2026-04-28 17:48:26 -0300

    feat(model): add usuario model

---
+++ src/main/java/com/classroomscheduler/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.classroomscheduler.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+}
