e9558c55802e4d186a36ab6aa3f33a89141016c2
djairofilho <djairodsfilho@gmail.com>
2026-04-28 17:48:27 -0300

    feat(model): add sala model

---
+++ src/main/java/com/classroomscheduler/model/Sala.java
@@ -0,0 +1,13 @@
+package com.classroomscheduler.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Sala extends Espaco {
+}
