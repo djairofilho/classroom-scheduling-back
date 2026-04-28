8f57f14965e1edb434d228eff0b8f7271df43462
djairofilho <djairodsfilho@gmail.com>
2026-04-28 20:19:25 -0300

    feat(dto): add create solicitante request

---
+++ src/main/java/com/classroomscheduler/dto/CreateSolicitanteRequest.java
@@ -0,0 +1,27 @@
+package com.classroomscheduler.dto;
+
+public class CreateSolicitanteRequest {
+
+    private String nome;
+
+    private String email;
+
+    public CreateSolicitanteRequest() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
