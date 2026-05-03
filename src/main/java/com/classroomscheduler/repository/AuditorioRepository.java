c39a4ad3888078136be4a9671115520079eee92a
djairofilho <djairodsfilho@gmail.com>
2026-05-03 02:22:39 -0300

    chore(model): remove espaco subtype classes

--- src/main/java/com/classroomscheduler/repository/AuditorioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.classroomscheduler.repository;
-
-import com.classroomscheduler.model.Auditorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditorioRepository extends JpaRepository<Auditorio, Long> {
-}
--- src/main/java/com/classroomscheduler/repository/QuadraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.classroomscheduler.repository;
-
-import com.classroomscheduler.model.Quadra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuadraRepository extends JpaRepository<Quadra, Long> {
-}
