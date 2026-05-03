d00b82bc96baf9a4d05f79410606330f177a51e5
djairofilho <djairodsfilho@gmail.com>
2026-05-03 01:19:41 -0300

    refactor(exception): add api exception types

---
+++ src/main/java/com/classroomscheduler/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.classroomscheduler.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
