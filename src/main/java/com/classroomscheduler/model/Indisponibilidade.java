57a9c8177080617387627c0f3d285680aa1b74a4
Rafaelvlf <rafaelvlf1@al.insper.edu.br>
2026-05-04 16:03:26 -0300

    Refact: getter e setter Indisponibilidade com lombok

--- src/main/java/com/classroomscheduler/model/Indisponibilidade.java
+++ src/main/java/com/classroomscheduler/model/Indisponibilidade.java
@@ -7,10 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Indisponibilidade {
 
     @Id
@@ -34,47 +38,4 @@ public class Indisponibilidade {
     public Indisponibilidade() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Espaco getEspaco() {
-        return espaco;
-    }
-
-    public void setEspaco(Espaco espaco) {
-        this.espaco = espaco;
-    }
-
-    public LocalDateTime getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(LocalDateTime inicio) {
-        this.inicio = inicio;
-    }
-
-    public LocalDateTime getFim() {
-        return fim;
-    }
-
-    public void setFim(LocalDateTime fim) {
-        this.fim = fim;
-    }
-
-    public String getMotivo() {
-        return motivo;
-    }
-
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-
-    public Usuario getCriadaPor() {
-        return criadaPor;
-    }
-
-    public void setCriadaPor(Usuario criadaPor) {
-        this.criadaPor = criadaPor;
-    }
 }
