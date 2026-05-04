ed42ae1de01fdff5d29160f8a24891bce5c6cb5b
Rafaelvlf <rafaelvlf1@al.insper.edu.br>
2026-05-04 16:06:45 -0300

    Refact: getter e setter Predio com lombok

--- src/main/java/com/classroomscheduler/model/Predio.java
+++ src/main/java/com/classroomscheduler/model/Predio.java
@@ -7,11 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Predio {
 
     @Id
@@ -31,43 +35,4 @@ public class Predio {
     public Predio() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getLocalizacao() {
-        return localizacao;
-    }
-
-    public void setLocalizacao(String localizacao) {
-        this.localizacao = localizacao;
-    }
-
-    public List<HorarioFuncionamento> getHorariosFuncionamento() {
-        return horariosFuncionamento;
-    }
-
-    public void setHorariosFuncionamento(List<HorarioFuncionamento> horariosFuncionamento) {
-        this.horariosFuncionamento = horariosFuncionamento;
-    }
 }
